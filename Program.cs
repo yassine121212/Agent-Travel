d7798dc876c24618cf713e454ef52afc133cf1f0
yassine121212 <elouazzaniyassine87@gmail.com>
2022-12-26 18:29:30 +0100

    Add route

--- Program.cs
+++ Program.cs
@@ -20,8 +20,15 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        endpoints.MapControllerRoute(
+            name: "myroute",
+            pattern: "myroute/{controller=Home}/{action=Index}/{id?}");
+    });
 
 app.Run();
