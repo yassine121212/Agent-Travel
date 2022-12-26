967b4c9c5149a1d1738e94c98b07566c26ad64bd
messimaha <70766603+messimaha@users.noreply.github.com>
2022-12-26 23:15:21 +0100

    ajouter header dans le fichier_layout

---
+++ Controllers/ClientController.cshtml.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using dotn.Models;
+
+namespace dotn.Controllers;
+
+public class ClientController : Controller
+{
+    private readonly ILogger<ClientController> _logger;
+
+    public ClientController(ILogger<ClientController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
--- Program.cs
+++ Program.cs
@@ -20,8 +20,15 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        endpoints.MapControllerRoute(
+            name: "Client",
+            pattern: "{controller=Client}/{action=Index}/{id?}");
+    });
 
 app.Run();
