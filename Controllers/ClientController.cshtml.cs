6962f0c0ee945f3361e31adecad83ce894fe9efb
messimaha <70766603+messimaha@users.noreply.github.com>
2022-12-29 11:54:57 +0100

    page login

--- Controllers/ClientController.cshtml.cs
+++ Controllers/ClientController.cshtml.cs
@@ -22,7 +22,14 @@ public class ClientController : Controller
     {
         return View();
     }
-
+    public IActionResult Login()
+    {
+        return View();
+    }
+    public IActionResult Register()
+    {
+        return View();
+    }
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
